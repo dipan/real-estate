903e90fdb3a168a1f8962bfff6fe7d02bbeb9a42
dipan <mandal.common@gmail.com>
2024-09-23 00:32:34 +0530

    doc: name update

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,11 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
-  var d = new Date(); 
-    let currentYear = d.getFullYear();
+  var d = new Date();
+  let currentYear = d.getFullYear();
   return (
     <>
-     
       <footer
         className="wow fadeInUp dark:bg-gray-dark relative z-10 bg-white pt-16 md:pt-20 lg:pt-24"
         data-wow-delay=".1s"
@@ -33,7 +32,7 @@ const Footer = () => {
                   />
                 </Link>
                 <p className="dark:text-body-color-dark mb-9 text-base leading-relaxed text-body-color">
-                Example
+                  18, Ellora Road Survey Park Kolkata 700075
                 </p>
                 <div className="flex items-center">
                   <a
@@ -202,7 +201,8 @@ const Footer = () => {
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color dark:text-white">
-            &#x24B8; Copyright {currentYear}, Example{" "}All Right Reserved
+              &#x24B8; Copyright {currentYear}, Maa Kali Construction All Right
+              Reserved
             </p>
           </div>
         </div>
